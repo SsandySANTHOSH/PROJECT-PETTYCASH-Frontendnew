39eaae24a0fbbd3e65b043151d5901af16bb7d1d
SsandySANTHOSH <santhoshs4717@gmail.com>
2023-09-18 10:39:03 +0530

    commit

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
 
 const Layout = ({ children }) => {
   return (
     <>
-        <Header />
-        <div className='content'>
-             {children}
-        </div>
-        <Footer />
-
+      <Header />
+      <div className="content container mt-4">{children}</div>
+      <Footer />
     </>
   );
 };
